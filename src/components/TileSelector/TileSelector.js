875e5d9b1d61329b3cde3b6f364b6206262fdd2b
Sven <sven.m.gerhards@gmail.com>
2023-04-05 16:06:10 +1200

    Module 1 - MOre changes

--- src/components/TileSelector/TileSelector.js
+++ src/components/TileSelector/TileSelector.js
@@ -1,23 +1,26 @@
 import React from 'react'
+import useHover from '../../hooks'
 import './TileSelector.css'
 
-const TileSelector = () => {
+const TileSelector = (props) => {
 
+  const [ref, hovered] = useHover()
 
-  const dropdown = (
+  const dropDown = hovered ? (
         <div className='tileSelectorContent' >
-          <div className='number'>4</div>
-          <div className='number'>16</div>
-          <div className='number'>36</div>
+          <div className='number' onClick={_ => props.handleNumTileChange(4)}>4</div>
+          <div className='number' onClick={_ => props.handleNumTileChange(16)}>16</div>
+          <div className='number' onClick={_ => props.handleNumTileChange(36)}>36</div>
         </div>
-  )
+  ) : null
 
  return (
    <div className='tileSelector'>
-     <div>Number of Tiles</div>
-     <div className='tileSelectorDropdown'>
-       {dropdown}
-     </div>
+      <div>Number of Tiles</div>
+      <div className='tileSelectorDropdown' ref={ref}>
+        {props.numTiles}
+        {dropDown}
+   </div>
    </div>
  )
 }
